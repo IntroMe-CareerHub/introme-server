a5cecc5d3d3f43b67f5179a35513529421521fc9
philos1234 <ohhyun1948@gmail.com>
2024-02-06 22:26:11 +0900

    init

---
+++ intro-me-interface/src/main/java/com/introme/intromeinterface/IntroMeInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.introme.intromeinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroMeInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntroMeInterfaceApplication.class, args);
+    }
+
+}
---
+++ intro-me-interface/src/test/java/com/introme/intromeinterface/IntroMeInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.introme.intromeinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroMeInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
