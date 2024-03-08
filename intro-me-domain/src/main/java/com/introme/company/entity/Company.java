82c41ec221120375268219841a9c85e1e830410c
hyebin <xxico89@gmail.com>
2024-03-08 15:19:55 +0900

    Feat: Company DTO -> Company Entity 변환 기능 추가

--- intro-me-domain/src/main/java/com/introme/company/entity/Company.java
+++ intro-me-domain/src/main/java/com/introme/company/entity/Company.java
@@ -1,22 +1,42 @@
 package com.introme.company.entity;
 
+import com.introme.company.dto.CompanyDTO;
 import jakarta.persistence.*;
+import lombok.*;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
 import java.util.Map;
 
+@Getter
 @Entity
+@NoArgsConstructor
 public class Company {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(unique = true, nullable = false)
     private String name;
-    @Column(nullable = false)
+    @Column()
     private String image;
 
     @JdbcTypeCode(SqlTypes.JSON)
-    @Column(nullable = false)
+    @Column()
     private Map<String, Object> information;
+
+    @Builder
+    public Company(Long id, String name, String image, Map<String, Object> information) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.information = information;
+    }
+
+    public static Company toEntity(CompanyDTO companyDTO) {
+        return Company.builder()
+                .name(companyDTO.getName())
+                .image(companyDTO.getImage())
+                .information(companyDTO.getInformation())
+                .build();
+    }
 }
