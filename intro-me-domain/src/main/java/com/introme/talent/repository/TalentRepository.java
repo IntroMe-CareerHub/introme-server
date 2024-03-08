d11ff4f5aa41a31c4bb6b75f0509062149d3dac6
hyebin <xxico89@gmail.com>
2024-03-08 15:21:08 +0900

    Feat: Talent 데이터 저장 API 서비스 추가

---
+++ intro-me-domain/src/main/java/com/introme/talent/repository/TalentRepository.java
@@ -0,0 +1,9 @@
+package com.introme.talent.repository;
+
+import com.introme.talent.entity.Talent;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TalentRepository extends JpaRepository<Talent, Long> {
+}
