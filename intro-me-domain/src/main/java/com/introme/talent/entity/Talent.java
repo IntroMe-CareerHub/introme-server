22992f820149d97c0b562df80d4302e7753f50db
hyebin <xxico89@gmail.com>
2024-03-08 15:19:42 +0900

    Feat: Talent DTO -> Talent Entity 변환 기능 추가

--- intro-me-domain/src/main/java/com/introme/talent/entity/Talent.java
+++ intro-me-domain/src/main/java/com/introme/talent/entity/Talent.java
@@ -1,9 +1,15 @@
 package com.introme.talent.entity;
 
 import com.introme.company.entity.Company;
+import com.introme.talent.dto.TalentDTO;
 import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
 @Entity
+@NoArgsConstructor
 public class Talent {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -12,7 +18,25 @@ public class Talent {
     @Column(nullable = false)
     private String keyword;
 
+    private String description;
+
     @ManyToOne
     @JoinColumn(name = "companyId")
     private Company company;
+
+    @Builder
+    public Talent(Long id, String keyword, String description, Company company) {
+        this.id = id;
+        this.keyword = keyword;
+        this.description = description;
+        this.company = company;
+    }
+
+    public static Talent toEntity(TalentDTO talentDTO, Company company) {
+        return Talent.builder()
+                .keyword(talentDTO.getKeyword())
+                .description(talentDTO.getDescription())
+                .company(company)
+                .build();
+    }
 }
