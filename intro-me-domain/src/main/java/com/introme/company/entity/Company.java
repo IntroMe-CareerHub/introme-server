f45396c87627fea8f8925908b05a9319490f51ae
hyebin <xxico89@gmail.com>
2024-03-12 00:10:01 +0900

    Feat: 엔터티 필드 Json Converter 추가

--- intro-me-domain/src/main/java/com/introme/company/entity/Company.java
+++ intro-me-domain/src/main/java/com/introme/company/entity/Company.java
@@ -1,14 +1,14 @@
 package com.introme.company.entity;
 
-import com.introme.company.dto.CompanyDTO;
+import com.introme.company.CompanyInfoJsonConverter;
+import com.introme.company.dto.request.CompanyReqDTO;
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
 
 import java.util.Map;
 
 @Getter
+@Setter
 @Entity
 @NoArgsConstructor
 public class Company {
@@ -17,26 +17,25 @@ public class Company {
     private Long id;
     @Column(unique = true, nullable = false)
     private String name;
-    @Column()
+    @Column
     private String image;
 
-    @JdbcTypeCode(SqlTypes.JSON)
-    @Column()
-    private Map<String, Object> information;
+    @Convert(converter = CompanyInfoJsonConverter.class)
+    @Column
+    private CompanyInfo information;
 
     @Builder
-    public Company(Long id, String name, String image, Map<String, Object> information) {
-        this.id = id;
+    public Company(String name, String image, CompanyInfo information) {
         this.name = name;
         this.image = image;
         this.information = information;
     }
 
-    public static Company toEntity(CompanyDTO companyDTO) {
+    public static Company toEntity(CompanyReqDTO companyReqDTO) {
         return Company.builder()
-                .name(companyDTO.getName())
-                .image(companyDTO.getImage())
-                .information(companyDTO.getInformation())
+                .name(companyReqDTO.getName())
+                .image(companyReqDTO.getImage())
+                .information(companyReqDTO.getInformation())
                 .build();
     }
 }
---
+++ intro-me-domain/src/main/java/com/introme/company/entity/CompanyInfo.java
@@ -0,0 +1,16 @@
+package com.introme.company.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CompanyInfo {
+    private String location;
+    private String url;
+    private String phoneNumber;
+}
