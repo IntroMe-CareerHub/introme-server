4db174e12ec7af157dff6cd841cf48a509373b1b
hyebin <xxico89@gmail.com>
2024-03-25 02:08:23 +0900

    Feat: 기업 인재상 등록하기 API 서비스 추가

--- intro-me-domain/src/main/java/com/introme/company/CompanyService.java
+++ intro-me-domain/src/main/java/com/introme/company/CompanyService.java
@@ -1,17 +1,24 @@
 package com.introme.company;
 
+import com.introme.company.dto.request.CompanyAddReqDTO;
 import com.introme.company.dto.request.CompanyReqDTO;
+import com.introme.company.dto.response.CompanyAddResDTO;
 import com.introme.company.entity.Company;
 import com.introme.company.repository.CompanyRepository;
+import com.introme.talent.entity.Talent;
+import com.introme.talent.entity.TalentInfo;
+import com.introme.talent.repository.TalentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class CompanyService {
     private final CompanyRepository companyRepository;
+    private final TalentRepository talentRepository;
 
     public Company save(CompanyReqDTO companyReqDTO) {
         return companyRepository.save(Company.toEntity(companyReqDTO));
@@ -19,5 +26,20 @@ public class CompanyService {
 
     public List<Company> findAllCompany() {
         return companyRepository.findAll();
+    }
+
+    public CompanyAddResDTO addCompanyTalents(CompanyAddReqDTO companyAddReqDTO) {
+        Company company = Company.toEntity(companyAddReqDTO);
+        companyRepository.save(company);
+
+        List<TalentInfo> talentInfos = companyAddReqDTO.getTalentInfo();
+        List<Talent> talents = new ArrayList<>();
+
+        for (TalentInfo talentInfo : talentInfos) {
+            Talent talent = Talent.toEntity(talentInfo, company);
+            talents.add(talentRepository.save(talent));
+        }
+
+        return new CompanyAddResDTO(company, talents);
     }
 }
