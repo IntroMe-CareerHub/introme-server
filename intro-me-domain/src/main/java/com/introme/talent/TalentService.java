680a183ebbfc2ea35c7d875d585979578fce63c8
hyebin <xxico89@gmail.com>
2024-03-25 00:18:54 +0900

    Feat: 특정 기업 인재상 조회하기 API 구현

--- intro-me-domain/src/main/java/com/introme/talent/TalentService.java
+++ intro-me-domain/src/main/java/com/introme/talent/TalentService.java
@@ -3,10 +3,13 @@ package com.introme.talent;
 import com.introme.company.entity.Company;
 import com.introme.company.repository.CompanyRepository;
 import com.introme.talent.dto.request.TalentReqDTO;
+import com.introme.talent.dto.response.TalentResDTO;
 import com.introme.talent.entity.Talent;
 import com.introme.talent.repository.TalentRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @AllArgsConstructor
 @Service
@@ -19,5 +22,9 @@ public class TalentService {
                 .orElseThrow(() -> new IllegalArgumentException("Company not found with this id:" + talentReqDTO.getCompanyId()));
 
         return talentRepository.save(Talent.toEntity(talentReqDTO, company));
+    }
+
+    public List<TalentResDTO> getTalentsByCompanyId(Long companyId) {
+        return talentRepository.findByCompanyId(companyId);
     }
 }
