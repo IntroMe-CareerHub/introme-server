83d09e9842e7de9502c5002fed3574939e097ded
hyebin <xxico89@gmail.com>
2024-04-15 15:44:28 +0900

    Feat: User 엔터티 생성

---
+++ intro-me-domain/src/main/java/com/introme/user/entity/IntroMeUser.java
@@ -0,0 +1,41 @@
+package com.introme.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class IntroMeUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public IntroMeUser(Long id, String name, String email, Role role) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.role = role;
+    }
+
+    public IntroMeUser update(String name) {
+        this.name = name;
+
+        return this;
+    }
+}
