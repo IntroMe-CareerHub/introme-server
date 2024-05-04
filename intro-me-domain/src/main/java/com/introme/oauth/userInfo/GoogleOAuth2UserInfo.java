4990a7f16ee0ff2d58d81480b7e726398846c7da
hyebin <xxico89@gmail.com>
2024-05-05 00:21:45 +0900

    Feat: Oauth2UserInfo, Google용 userInfo 추가

---
+++ intro-me-domain/src/main/java/com/introme/oauth/userInfo/GoogleOAuth2UserInfo.java
@@ -0,0 +1,29 @@
+package com.introme.oauth.userInfo;
+
+import java.util.Map;
+
+public class GoogleOAuth2UserInfo extends Oauth2UserInfo {
+    public GoogleOAuth2UserInfo(Map<String, Object> attributes) {
+        super(attributes);
+    }
+
+    @Override
+    public String getId() {
+        return (String) attributes.get("sub");
+    }
+
+    @Override
+    public String getName() {
+        return (String) attributes.get("name");
+    }
+
+    @Override
+    public String getEmail() {
+        return (String) attributes.get("email");
+    }
+
+    @Override
+    public String getPicture() {
+        return (String) attributes.get("picture");
+    }
+}
