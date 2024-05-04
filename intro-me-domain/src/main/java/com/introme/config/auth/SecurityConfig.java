e76e4619eb1c47564dfcf35f7a61210c3e915f1d
hyebin <xxico89@gmail.com>
2024-05-04 23:48:27 +0900

    Chore: SecurityConfig 파일 수정

--- intro-me-domain/src/main/java/com/introme/config/auth/SecurityConfig.java
+++ intro-me-domain/src/main/java/com/introme/config/auth/SecurityConfig.java
@@ -1,11 +1,12 @@
 package com.introme.config.auth;
 
+import com.introme.oauth.handler.OAuth2LoginFailureHandler;
+import com.introme.oauth.handler.OAuth2LoginSuccessHandler;
 import com.introme.user.CustomOAuth2UserService;
 import com.introme.user.entity.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
@@ -15,12 +16,14 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig{
+public class SecurityConfig {
     private final CustomOAuth2UserService customOAuth2UserService;
+    private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
+    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        // TODO: 코드 돌아가는 흐름 깨우치기 & 리팩토링
+
         httpSecurity
                 .csrf(CsrfConfigurer::disable)
                 .headers((headerConfig) -> headerConfig.frameOptions(
@@ -28,16 +31,14 @@ public class SecurityConfig{
                 ))
                 .authorizeHttpRequests((authorizeRequest) ->
                         authorizeRequest
-                                .requestMatchers("/").permitAll()
-                                .requestMatchers("/api/v1/company/**").hasAnyRole(Role.User.name())
+                                .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/favicon.ico", "/h2-console/**").permitAll()
                                 .requestMatchers("/admin/**").hasAnyRole(Role.ADMIN.name())
                                 .anyRequest().authenticated())
                 .oauth2Login((oauth2) -> oauth2
+                        .successHandler(oAuth2LoginSuccessHandler)
+                        .failureHandler(oAuth2LoginFailureHandler)
                         .userInfoEndpoint((userInfoEndpointConfig -> userInfoEndpointConfig
-                                .userService(customOAuth2UserService)))
-                        .defaultSuccessUrl("/oauth/login"))
-                .logout((logoutConfig) ->
-                        logoutConfig.logoutSuccessUrl("/"));
+                                .userService(customOAuth2UserService))));
 
         return httpSecurity.build();
     }
