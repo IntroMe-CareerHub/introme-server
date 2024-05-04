a2d46001ba767e70197e790d5cd36b3e31c3c16a
hyebin <xxico89@gmail.com>
2024-05-04 23:49:59 +0900

    Feat: CustomOAuth2User 추가

--- intro-me-domain/src/main/java/com/introme/user/CustomOAuth2UserService.java
+++ intro-me-domain/src/main/java/com/introme/user/CustomOAuth2UserService.java
@@ -1,18 +1,18 @@
 package com.introme.user;
 
+import com.introme.oauth.SocialType;
 import com.introme.user.entity.IntroMeUser;
-import com.introme.user.entity.OAuthAttributes;
-import com.introme.user.entity.Role;
-import com.introme.user.entity.SessionUser;
+import com.introme.oauth.OAuthAttributes;
 import com.introme.user.repository.UserRepository;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
@@ -21,35 +21,44 @@ import java.util.Collections;
 @RequiredArgsConstructor
 @Service
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+    private static final Logger log = LoggerFactory.getLogger(CustomOAuth2UserService.class);
     private final UserRepository userRepository;
     private final HttpSession httpSession;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        log.info("loadUser() 실행 - OAuth2 로그인 요청 진입");
         OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
         OAuth2User oAuth2User = delegate.loadUser(userRequest);
 
+        SocialType socialType = SocialType.GOOGLE;
         String userNameAttributeName = userRequest.getClientRegistration()
                 .getProviderDetails()
                 .getUserInfoEndpoint()
                 .getUserNameAttributeName();
 
-        OAuthAttributes attributes = OAuthAttributes.of(userNameAttributeName, oAuth2User.getAttributes());
-        IntroMeUser user = saveOrUpdate(attributes);
-        // save user information in session
-        httpSession.setAttribute("user", new SessionUser(user));
+        OAuthAttributes attributes = OAuthAttributes.of(socialType, userNameAttributeName, oAuth2User.getAttributes());
 
-        return new DefaultOAuth2User(
-                Collections.singleton(new SimpleGrantedAuthority(Role.User.getKey())),
-                attributes.getAttributes(),
-                attributes.getNameAttributeKey());
+        IntroMeUser createdUser = getUser(attributes, socialType);
+
+        return new CustomOAuth2User(
+                Collections.singleton(new SimpleGrantedAuthority(createdUser.getRole().getKey())),
+                oAuth2User.getAttributes(),
+                attributes.getNameAttributeKey(),
+                createdUser.getEmail(),
+                createdUser.getRole());
     }
 
-    private IntroMeUser saveOrUpdate(OAuthAttributes attributes) {
-        IntroMeUser user = userRepository.findByEmail(attributes.getEmail())
-                .map(entity -> entity.update(attributes.getName()))
-                // 가입하지 않은 사용자면, User 엔터티 생성
-                .orElse(attributes.toEntity());
+    private IntroMeUser getUser(OAuthAttributes attributes, SocialType socialType) {
+        IntroMeUser findUser = userRepository.findBySocialTypeAndSocialId(socialType, attributes.getOauth2UserInfo().getId()).orElse(null);
+        if (findUser == null) {
+            return saveUser(attributes, socialType);
+        }
+        return findUser;
+    }
+
+    private IntroMeUser saveUser(OAuthAttributes attributes, SocialType socialType) {
+        IntroMeUser user = attributes.toEntity(socialType, attributes.getOauth2UserInfo());
 
         return userRepository.save(user);
     }
