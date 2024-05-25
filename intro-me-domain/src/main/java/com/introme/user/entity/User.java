b1dde8789a8c25b2d60f3960aa33fa3035a88a8c
philos1234 <ohhyun1948@gmail.com>
2024-05-25 22:38:16 +0900

    User

---
+++ intro-me-domain/src/main/java/com/introme/user/entity/User.java
@@ -0,0 +1,28 @@
+package com.introme.user.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
+
--- intro-me-domain/src/main/java/com/introme/user/service/UserService.java
+++ intro-me-domain/src/main/java/com/introme/user/service/UserService.java
@@ -1,8 +1,5 @@
 package com.introme.user.service;
 
-import com.introme.oauth.SocialType;
-import com.introme.oauth.userInfo.Oauth2UserInfo;
-import com.introme.user.entity.IntroMeUser;
 import com.introme.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,17 +9,5 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private final UserRepository userRepository;
 
-    public IntroMeUser getUser(Oauth2UserInfo oauth2UserInfo, SocialType socialType) {
-        IntroMeUser findUser = userRepository.findBySocialTypeAndSocialId(socialType, oauth2UserInfo.getId()).orElse(null);
-        if (findUser == null) {
-            return saveUser(oauth2UserInfo, socialType);
-        }
-        return findUser;
-    }
 
-    private IntroMeUser saveUser(Oauth2UserInfo oauth2UserInfo, SocialType socialType) {
-        IntroMeUser user = IntroMeUser.toEntity(socialType, oauth2UserInfo);
-
-        return userRepository.save(user);
-    }
 }
