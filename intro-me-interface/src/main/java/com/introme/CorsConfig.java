99e3d23e782254cef42b05aea252217be0fea95e
hyebin <xxico89@gmail.com>
2024-06-06 19:12:13 +0900

    Setting: Cors Config 설정

---
+++ intro-me-interface/src/main/java/com/introme/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.introme;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
